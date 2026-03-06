6a7d18883d5bc5085e3066da7fedb1dbfe92742c
Lionel <93621149+lionelmarcus10@users.noreply.github.com>
2026-03-06 16:01:21 +0000

    Final pipeline

--- src/main.c
+++ src/main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]){
 	else {printf("Erreur de parametres");}
 	}
 
-	else if (argc == 3 && strcmp(argv[1], "square") == 0) {
+	else if (argc == 3 && strcmp(argv[1], "car") == 0) {
         char* a = argv[2];
         double r = _car(atof(a));
         printf("%lf", r);
